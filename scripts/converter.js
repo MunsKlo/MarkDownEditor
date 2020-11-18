0bcfedabe7bd87943fdfd37b8d0ca9a394dc7df4
MunsKlo <junkerg01@gmail.com>
2020-11-18 16:15:09 +0100

    Delete some lines

--- scripts/converter.js
+++ scripts/converter.js
@@ -6,8 +6,6 @@ function convertToHTMl(txt){
 function fillDiv(){
     var htmlMd = convertToHTMl(document.getElementById("inputfield").value)
     document.getElementById("markdown-text").innerHTML = htmlMd
-    console.log(htmlMd)
 }
 
-console.log('converter.js')
 document.getElementById("inputfield").addEventListener('input', () => fillDiv())
